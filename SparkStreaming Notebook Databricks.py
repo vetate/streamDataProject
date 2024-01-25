63b0ba934a151a1de32a7e73ccb905e5c874803c
vtate@serc.ac.uk <vtate@serc.ac.uk>
2024-01-25 13:17:21 +0000

    stream data

--- SparkStreaming Notebook Databricks.py
+++ SparkStreaming Notebook Databricks.py
@@ -1,23 +1,90 @@
 # Databricks notebook source
+from pyspark.sql.functions import *
 from pyspark.sql.types import *
-import pyspark.sql.functions as F
-from datetime import datetime as dt
-import json
 
+# COMMAND ----------
+
+try:
+    spark.sql("create catalog streaming;")
+except:
+    print('check if catalog already exists')
+
+try:
+    spark.sql("create schema streaming.bronze;")
+except:
+    print('check if bronze schema already exists')
+
+try:
+    spark.sql("create schema streaming.silver")
+except:
+    print('check if silver schema already exists')
+
+try:
+    spark.sql("create schema streaming.gold;")
+except:
+    print('check if gold schema already exists')
+
+# COMMAND ----------
+
+try:
+    spark.sql("create catalog streaming;")
+except:
+    print('check if catalog already exists')
+
+try:
+    spark.sql("create schema streaming.bronze;")
+except:
+    print('check if bronze schema already exists')
+
+try:
+    spark.sql("create schema streaming.silver")
+except:
+    print('check if silver schema already exists')
+
+try:
+    spark.sql("create schema streaming.gold;")
+except:
+    print('check if gold schema already exists')
+
+# COMMAND ----------
+
+
+# Config
+# Replace with your Event Hub namespace, name, and key
 connectionString = "Endpoint=sb://eh-namespace-stream.servicebus.windows.net/;SharedAccessKeyName=ve-policy;SharedAccessKey=RAjOTKnsI8CrdWbrR/hLi+yyOjZozPsUy+AEhLX586c=;EntityPath=eh-streamproject"
-ehConf = {}
-ehConf["eventhubs.connectionString"] = connectionString
-ehConf["eventhubs.consumerGroup"] = "$Default"
+eventHubName = "eh-streamproject"
 
-json_schema = StructType(
-    [
-        StructField("deviceID", IntegerType(), True),
-        StructField("rpm", IntegerType(), True),
-        StructField("angle", IntegerType(), True),
-        StructField("humidity", IntegerType(), True),
-        StructField("windspeed", IntegerType(), True),
-        StructField("temperature", IntegerType(), True),
-        StructField("deviceTimestamp", StringType(), True),
-        StructField("deviceDate", StringType(), True),
-    ]
+ehConf = {
+  'eventhubs.connectionString' : sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(connectionString),
+  'eventhubs.eventHubName': eventHubName
+}
+   
+
+# COMMAND ----------
+
+
+# Reading stream: Load data from Azure Event Hub into DataFrame 'df' using the previously configured settings
+df = spark.readStream \
+    .format("eventhubs") \
+    .options(**ehConf) \
+    .load() \
+
+# Displaying stream: Show the incoming streaming data for visualization and debugging purposes
+df.display()
+
+# COMMAND ----------
+
+dbutils.fs.mount(
+    source='wasbs://streaming@moviesdatasa.blob.core.windows.net',
+    mount_point='/mnt/streaming',
+    extra_configs = {'fs.azure.account.key.moviesdatasa.blob.core.windows.net': dbutils.secrets.get('moviesScopeProject', 'storageAccountKey')}
 )
+
+# COMMAND ----------
+
+# Writing stream: Persist the streaming data to a Delta table 'streaming.bronze.weather' in 'append' mode with checkpointing
+df.writeStream\
+    .option("checkpointLocation", "/mnt/streaming/bronze/weather")\
+    .outputMode("append")\
+    .format("delta")\
+    .toTable("streaming.bronze.weather")
