34fc8fa3c43904578c20cdc2d976d1e5e2e9195c
vtate@serc.ac.uk <vtate@serc.ac.uk>
2024-01-25 14:30:22 +0000

    saved

--- SparkStreaming Notebook Databricks.py
+++ SparkStreaming Notebook Databricks.py
@@ -26,28 +26,6 @@ except:
 
 # COMMAND ----------
 
-try:
-    spark.sql("create catalog streaming;")
-except:
-    print('check if catalog already exists')
-
-try:
-    spark.sql("create schema streaming.bronze;")
-except:
-    print('check if bronze schema already exists')
-
-try:
-    spark.sql("create schema streaming.silver")
-except:
-    print('check if silver schema already exists')
-
-try:
-    spark.sql("create schema streaming.gold;")
-except:
-    print('check if gold schema already exists')
-
-# COMMAND ----------
-
 
 # Config
 # Replace with your Event Hub namespace, name, and key
@@ -62,18 +40,6 @@ ehConf = {
 
 # COMMAND ----------
 
-
-# Reading stream: Load data from Azure Event Hub into DataFrame 'df' using the previously configured settings
-df = spark.readStream \
-    .format("eventhubs") \
-    .options(**ehConf) \
-    .load() \
-
-# Displaying stream: Show the incoming streaming data for visualization and debugging purposes
-df.display()
-
-# COMMAND ----------
-
 dbutils.fs.mount(
     source='wasbs://streaming@moviesdatasa.blob.core.windows.net',
     mount_point='/mnt/streaming',
@@ -82,9 +48,32 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-# Writing stream: Persist the streaming data to a Delta table 'streaming.bronze.weather' in 'append' mode with checkpointing
-df.writeStream\
-    .option("checkpointLocation", "/mnt/streaming/bronze/weather")\
-    .outputMode("append")\
-    .format("delta")\
-    .toTable("streaming.bronze.weather")
+dbutils.fs.chmod(
+    "/mnt/streaming/streaming.bronze/",  # Path to the directory
+    "775"  # Octal notation for permissions (user: read, write, execute; group: read, write, execute; others: read, execute)
+)
+
+# COMMAND ----------
+
+# Reading stream: Load data from Azure Event Hub into DataFrame 'df' using the previously configured settings
+df = spark.readStream \
+    .format("eventhubs") \
+    .options(**ehConf) \
+    .load()
+
+# Displaying stream: Show the incoming streaming data for visualization and debugging purposes
+df.display()
+
+# Write stream to Hive table in Parquet format
+checkpoint_location = "/mnt/streaming/checkpoints/"
+query_name = "streaming_query"
+
+
+# Write stream to Hive table in Parquet format
+df.writeStream \
+    .format("parquet") \
+    .option("path", "/mnt/streaming/streaming.bronze/") \
+    .option("checkpointLocation", "/mnt/streaming/checkpoints/") \
+    .trigger(processingTime="60 seconds") \
+    .start() \
+    .awaitTermination()
